dc63b0e9bade84eaadb6eb541d69c99ab36cf8f4
GrandpaRex <77104671+GrandpaRex@users.noreply.github.com>
2023-06-01 22:35:50 -0500

    Initial commit

---
+++ ResponseCode/ResponseCode/Common.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CitizenFX.Core.Native.API;
+
+namespace ResponseCode
+{
+    internal static class Common
+    {
+        internal static void DisplayNotification(string text)
+        {
+            BeginTextCommandThefeedPost("STRING");
+            AddTextComponentString(text);
+            EndTextCommandThefeedPostTicker(true, true);
+        }
+    }
+}
---
+++ ResponseCode/ResponseCode/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using FivePD.API;
+using FivePD.API.Utils;
+using CitizenFX.Core.Native;
+
+#pragma warning disable 1998
+namespace ResponseCode
+{
+    public class Main : Plugin
+    {
+        internal Main() 
+        {
+            Events.OnCalloutAccepted += new Events.OnCalloutAcceptedDelegate(this.OnCalloutAccepted);
+        }
+
+        public async Task OnCalloutAccepted(Callout callout)
+        {
+            int callResponse = callout.ResponseCode;
+            if (callResponse == 1) { Common.DisplayNotification($"Respond code ~w~1"); }
+            if (callResponse == 2) { Common.DisplayNotification($"Respond code ~y~2"); }
+            if (callResponse == 3) { Common.DisplayNotification($"Respond code ~r~3"); }
+        }
+    }
+}
